229730e82ae10fbac6fb64c740484e6483e730e3
Jithesh <96974839+Jithesh-Poojari@users.noreply.github.com>
2024-03-02 21:50:56 +0530

    created model

---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import joblib
+
