95588bd99682fae7acf80cebc95830b15ec6d556
Jithesh <96974839+Jithesh-Poojari@users.noreply.github.com>
2024-03-02 21:52:59 +0530

    removed no use file

--- main.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-import joblib
-
